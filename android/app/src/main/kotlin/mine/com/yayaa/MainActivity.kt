bc93fa748e7b1fa46976f71e11aec137689c5c81
yayaa.rmh <ramadhaniyayaa12@gmail.com>
2025-03-13 23:12:22 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/mine/com/yayaa/MainActivity.kt
@@ -0,0 +1,5 @@
+package mine.com.yayaa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:yayaa/firebase_options.dart';
+import 'package:yayaa/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 237, 234, 19)),
+        useMaterial3: true,
+      ),
+      home: const RegisterView(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  print('You are a verified user');
+                } else {
+                  print('You need ti verify your email first');
+                }
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
+}
